08313582a282484ad971f411ff2487b4e3438978
Daniel Lenharo de Souza <daniel@sombra.eti.br>
2022-05-21 14:17:34 -0300

    Implementado multilinguas

--- hello.py
+++ hello.py
@@ -1,3 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-print("Hello, World")
+__version__ = "0.0.1"
+__author__ = "Daniel Lenharo"
+__license__ = "GPL-3"
+
+import os
+
+current_language = os.getenv("LANG","en_US")[:5]
+msg = "Hello, World"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
